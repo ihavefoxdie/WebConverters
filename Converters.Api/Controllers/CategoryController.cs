24f064a029ecdf87af8edae7202d9c9e410b6642
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2025-01-16 17:06:15 +0300

    Code refactor
    Refactor:
    -separated Category actions from ServiceController into the new CategoryController class.

---
+++ Converters.Api/Controllers/CategoryController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Converters.Models;
+using Converters.Api.Repositories.Interfaces;
+using Converters.Api.Entities;
+
+namespace Converters.Api.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryRepository<ServiceCategory> _categoryRepository;
+
+
+    public CategoryController(ICategoryRepository<ServiceCategory> categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+
+    #region Post
+    [HttpPost]
+    public async Task<ActionResult> AddCategory(string name)
+    {
+        try
+        {
+            ServiceCategory category = new()
+            {
+                Name = name
+            };
+            await _categoryRepository.AddCategory(category);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+        }
+
+        return Ok();
+    }
+    #endregion
+
+
+    #region Get
+    [HttpGet]
+    public async Task<ActionResult<ServiceCategoryDTO>> GetCategory(int id)
+    {
+        ServiceCategoryDTO itemDTO;
+        try
+        {
+            ServiceCategory? item = await _categoryRepository.GetCategory(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            itemDTO = new()
+            {
+                Id = item.Id,
+                Name = item.Name,
+            };
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                            "Internal server error: " + e.Message);
+        }
+
+        return Ok(itemDTO);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ServiceCategoryDTO>>> GetCategories()
+    {
+        List<ServiceCategoryDTO> itemsDTO = [];
+        try
+        {
+            IEnumerable<ServiceCategory>? items = await _categoryRepository.GetCategories();
+
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var item in items)
+            {
+                itemsDTO.Add(new ServiceCategoryDTO
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                            "Internal server error: " + e.Message);
+        }
+
+        return Ok(itemsDTO);
+    }
+    #endregion
+
+
+    #region Put
+    [HttpPut]
+    public async Task<ActionResult> UpdateCategory(int id, string name)
+    {
+        try
+        {
+            ServiceCategory changedCategory = new()
+            {
+                Name = name,
+                Id = id  
+            };
+            ServiceCategory? category = await _categoryRepository.UpdateCategory(changedCategory);
+
+            if (category is null)
+                return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+        }
+
+        return Ok();
+    }
+    #endregion
+
+
+    #region Delete
+    [HttpDelete]
+    public async Task<ActionResult> DeleteCategory(int id)
+    {
+        try
+        {
+            ServiceCategory? category = await _categoryRepository.DeleteCategory(id);
+
+            if (category is null)
+                return NotFound();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+        }
+
+        return Ok();
+    }
+    #endregion
+}
--- Converters.Api/Controllers/ServiceController.cs
+++ Converters.Api/Controllers/ServiceController.cs
@@ -2,24 +2,21 @@
 using Converters.Api.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Converters.Models;
-using Microsoft.AspNetCore.Http.Features;
-using System.ComponentModel;
 
 namespace Converters.Api.Controllers;
 
+//TODO: separate category related actions into a new controller
 [Route("api/[controller]/[action]")]
 [ApiController]
 public class ServiceController : ControllerBase
 {
     private readonly IServiceRepository<Service> _serviceRepository;
-    private readonly ICategoryRepository<ServiceCategory> _categoryRepository;
+
 
     public ServiceController(IServiceRepository<Service> serviceRepository, ICategoryRepository<ServiceCategory> categoryRepository)
     {
         _serviceRepository = serviceRepository;
-        _categoryRepository = categoryRepository;
     }
-
 
 
     #region Post
@@ -45,26 +42,8 @@ public class ServiceController : ControllerBase
 
         return Ok();
     }
-
-    [HttpPost]
-    public async Task<ActionResult> AddCategory(string name)
-    {
-        try
-        {
-            ServiceCategory category = new()
-            {
-                Name = name
-            };
-            await _categoryRepository.AddCategory(category);
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
-        }
-
-        return Ok();
-    }
     #endregion
+
 
     #region Get
     [HttpGet]
@@ -146,65 +125,6 @@ public class ServiceController : ControllerBase
 
         return Ok(itemsDTO);
     }
-
-    [HttpGet]
-    public async Task<ActionResult<ServiceCategoryDTO>> GetCategory(int id)
-    {
-        ServiceCategoryDTO itemDTO;
-        try
-        {
-            ServiceCategory? item = await _categoryRepository.GetCategory(id);
-
-            if (item == null)
-            {
-                return NotFound();
-            }
-
-            itemDTO = new()
-            {
-                Id = item.Id,
-                Name = item.Name,
-            };
-        }
-        catch (Exception e)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                            "Internal server error: " + e.Message);
-        }
-
-        return Ok(itemDTO);
-    }
-
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<ServiceCategoryDTO>>> GetCategories()
-    {
-        List<ServiceCategoryDTO> itemsDTO = [];
-        try
-        {
-            IEnumerable<ServiceCategory>? items = await _categoryRepository.GetCategories();
-
-            if (items == null)
-            {
-                return NotFound();
-            }
-
-            foreach (var item in items)
-            {
-                itemsDTO.Add(new ServiceCategoryDTO
-                {
-                    Id = item.Id,
-                    Name = item.Name,
-                });
-            }
-        }
-        catch (Exception e)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                            "Internal server error: " + e.Message);
-        }
-
-        return Ok(itemsDTO);
-    }
     #endregion
 
     #region Put
@@ -234,29 +154,6 @@ public class ServiceController : ControllerBase
 
         return Ok();
     }
-
-    [HttpPut]
-    public async Task<ActionResult> UpdateCategory(int id, string name)
-    {
-        try
-        {
-            ServiceCategory changedCategory = new()
-            {
-                Name = name,
-                Id = id  
-            };
-            ServiceCategory? category = await _categoryRepository.UpdateCategory(changedCategory);
-
-            if (category is null)
-                return NotFound();
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
-        }
-
-        return Ok();
-    }
     #endregion
 
     #region Delete
@@ -268,24 +165,6 @@ public class ServiceController : ControllerBase
             Service? service = await _serviceRepository.DeleteItem(id);
 
             if (service is null)
-                return NotFound();
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
-        }
-
-        return Ok();
-    }
-
-    [HttpDelete]
-    public async Task<ActionResult> DeleteCategory(int id)
-    {
-        try
-        {
-            ServiceCategory? category = await _categoryRepository.DeleteCategory(id);
-
-            if (category is null)
                 return NotFound();
         }
         catch (Exception ex)
